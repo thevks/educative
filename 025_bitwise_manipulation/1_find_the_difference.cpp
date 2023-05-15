555592e2038593b442a0abc95daf6cc064477e10
thevks <techikida@gmail.com>
2023-05-15 23:36:27 +0530

    Update 1_find_the_difference.cpp

--- 025_bitwise_manipulation/1_find_the_difference.cpp
+++ 025_bitwise_manipulation/1_find_the_difference.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <vector>
 
+int ExtraCharacterIndex(string firstString, string secondString)
+{
+    // Your code will replace this placeholder return statement
+    int result = 0;
+    int m = firstString.length();
+    int n = secondString.length();
+
+    int i;
+    for (i=0;i<m;i++)
+        result ^= int(firstString[i]);
+    
+    for (i=0;i<n;i++)
+        result ^= int(secondString[i]);
+
+    char c = char(result);
+
+    if (m > n) {
+        for (i=0;i<m;i++)
+            if (firstString[i] == c)
+                return i;
+    } else {
+        for (i=0;i<n;i++)
+            if (firstString[i] == c)
+                return i;
+    }
+
+    return -1;
+}
